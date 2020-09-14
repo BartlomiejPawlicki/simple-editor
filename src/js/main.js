1c86bbc5c16631e425a803701d0fe9109989642c
Bartlomiej Pawlicki <barpawlicki@gmail.com>
2020-09-14 23:58:04 +0200

    feature js local.storage

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,15 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  });
 }
 
 // place your code below
@@ -20,3 +20,17 @@ if ('serviceWorker' in navigator) {
 console.log(`Hello world!`);
 
 
+const message = document.querySelector('.form__message--js')
+const save = document.querySelector('.form__button--save-js')
+const load = document.querySelector('.form__button--load-js')
+
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.setItem('message', message.value)
+})
+
+
+load.addEventListener('click', (e) => {
+    e.preventDefault();
+    message.value = localStorage.getItem('message')
+})
